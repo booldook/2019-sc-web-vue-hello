bcb3954c4bd345f08e69abb67e8210fc6244826f
booldook <booldook@gmail.com>
2019-11-18 11:24:30 +0900

    v-for 구현

---
+++ js/index.js
@@ -0,0 +1,12 @@
+new Vue({
+	el: "#app",
+	data: {
+		title: "Vue를 배워봅시다.",
+		str: '',
+		items: [
+			{id:1, title:"연어", desc:"맛있는 연어", src:"../img/salmon.jpg"},
+			{id:1, title:"체리", desc:"싱싱한 체리", src:"../img/cherries.jpg"},
+			{id:1, title:"와인", desc:"오래된 와인", src:"../img/wine.jpg"},
+		]
+	}
+});
