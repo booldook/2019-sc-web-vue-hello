e443a084be69c17682868cbc2f4dc4bb0625728c
booldook <booldook@gmail.com>
2019-11-18 12:00:35 +0900

    v-on, v-bind

--- js/index.js
+++ js/index.js
@@ -3,10 +3,16 @@ new Vue({
 	data: {
 		title: "Vue를 배워봅시다.",
 		str: '',
+		isPrdView: false,
 		items: [
 			{id:1, title:"연어", desc:"맛있는 연어", src:"../img/salmon.jpg"},
 			{id:1, title:"체리", desc:"싱싱한 체리", src:"../img/cherries.jpg"},
 			{id:1, title:"와인", desc:"오래된 와인", src:"../img/wine.jpg"},
 		]
+	},
+	methods: {
+		onTogglePrd(e) {
+			this.isPrdView = !this.isPrdView;
+		},
 	}
 });
